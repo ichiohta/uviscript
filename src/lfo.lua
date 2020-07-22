06e066292a0906528b3fe866eb632e53e0e1330b
Ichiro Ota <ichiohta@live.com>
2020-07-22 04:38:54 -0700

    Support attack

--- src/lfo.lua
+++ src/lfo.lua
@@ -13,15 +13,17 @@ LFO.freq      = 4   -- Hz
 LFO.depth     = 0.1 -- [0..1]
 LFO.grain     = 5   -- msec
 LFO.elapsed   = 0   -- msec
+LFO.attack    = 0   -- msec
 
 function LFO:tick ()
     self.elapsed = self.elapsed + self.grain
 end
 
 function LFO:getModulation ()
-    local elapsed = math.max(self.elapsed - self.delay)
-    local phase = self.freq * math.max(elapsed, 0) / 1000
-    return self.depth * math.sin(2 * math.pi * phase)
+    local elapsed = math.max((self.elapsed - self.delay), 0)
+    local phase = self.freq * elapsed / 1000
+    local factor = self.attack > 0 and (math.min(elapsed, self.attack) / self.attack) or 1
+    return self.depth * factor * math.sin(2 * math.pi * phase)
 end
 
 function LFO:reset ()
