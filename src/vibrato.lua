6935cff4f218bae47029b6fb7ac35407ecbece64
Ichiro Ota <ichiohta@live.com>
2020-07-22 13:29:29 -0700

    More range

--- src/vibrato.lua
+++ src/vibrato.lua
@@ -9,10 +9,10 @@ require('differentiator')
 local lfo     = LFO.new()
 local tracker = DIFFERENTIATOR.new()
 
-Delay = Knob("Delay", 0, 0, 1000) -- 0 msec
+Delay = Knob("Delay", 0, 0, 5000) -- 0 msec
 Freq = Knob("Freq", 4.0, 0, 10) -- 4 Hz
-Depth = Knob("Depth", 0.1, 0, 2)
-Attack = Knob("Attack", 0, 0, 1000) -- 0 msec
+Depth = Knob("Depth", 0.1, 0, 5)
+Attack = Knob("Attack", 0, 0, 5000) -- 0 msec
 
 bindKnobsToLfo{lfo=lfo, delay=Delay, freq=Freq, depth=Depth, attack=Attack}
 
